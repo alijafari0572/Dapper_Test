fd3b6392d8c0319ba89768c68f9cea3abfe5615f
alijafari0572 <alijafari0572@gmail.com>
2023-01-07 00:07:50 +0330

    Add project files.

---
+++ Dapper_Test/ManagePeople.cs
@@ -0,0 +1,23 @@
+﻿using Dapper_Test.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Dapper_Test
+{
+    public class ManagePeople
+    {
+        private static string ConnectionString = "Data Source = .;Initial Catalog=Dapper_DB;Integrated Security=True;";
+   public static List<Person_Viewmodel> GetAll()
+        {
+
+             List<Person_Viewmodel> list;
+            using(IDbConnection db=new SqlConnection(ConnectionString))
+            {
+
+            }
+            return list;
+        }
+    }
+}
---
+++ Dapper_Test/Model/Person_Viewmodel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper_Test.Model
+{
+    public class Person_Viewmodel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string Website { get; set; }
+        public string Email { get; set;}
+    }
+}
